d6a5164cb9e2ba18802a9bfb6f9c197782b3a644
swapnil1393 <swapnilp1393@gmail.com>
2024-03-27 10:28:26 +0530

    Now Department file work also completed

--- Department.java
+++ Department.java
@@ -1,4 +1,10 @@
 public class Department
 {
-    
+    public void Departments()
+{
+  sytem.out.println("No of deoartments");
+  sytem.out.println("i am newly added file when programmer recollect the code");
+
+
+}
 }
